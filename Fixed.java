e1ae9bee52706c29f83e46e1ccad266a336cd9a0
Scott Burton <scotthburton@gmail.com>
2014-05-12 08:24:56 -0600

    working on the Fixed class

--- Fixed.java
+++ Fixed.java
@@ -1,4 +1,11 @@
-public class Fixed {
+public class Fixed implements Runnable {
+   @Override
+   public void run() {
+      for (int i = 0; i < 100; i++) {
+         System.out.println(i);
+      }
+   }
+
    public static void main(String[] args) {
       System.out.println("This is a great class.");
    }
