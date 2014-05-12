51d151204f027a155e3447240644bcf470ca4df5
Scott Burton <scotthburton@gmail.com>
2014-05-12 08:31:17 -0600

    passing params to broken

--- Broken.java
+++ Broken.java
@@ -1,5 +1,12 @@
 public class Broken {
+   public void run(String[] args) {
+      for (String arg : args) {
+         System.out.println(arg);
+      }
+   }
+
    public static void main(String[] args) {
       System.out.println("This is a great class.");
+      new Broken().run(args);
    }
 }
