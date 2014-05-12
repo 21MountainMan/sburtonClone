684843661822a1dd8db83a76a5c0fa55e8a19bbc
Scott Burton <scotthburton@gmail.com>
2014-05-12 08:37:07 -0600

    a few more changes

--- Broken.java
+++ Broken.java
@@ -1,11 +1,15 @@
 public class Broken {
-   public void run(String[] args) {
+   public void run(String[] args) throws Exception {
       for (String arg : args) {
          System.out.println(arg);
       }
+
+      if (args[0].equals("ex")) {
+         throw new Exception("ex was passed!");
+      }
    }
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws Exception {
       System.out.println("This is a great class.");
       new Broken().run(args);
    }
--- Deleted.java
+++
@@ -1,5 +0,0 @@
-public class Deleted {
-   public static void main(String[] args) {
-      // This file is not doing anything and really should be deleted.
-   }
-}
